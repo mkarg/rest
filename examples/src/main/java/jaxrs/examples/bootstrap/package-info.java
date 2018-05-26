8faa49474cfa6d201fec96b077a4ce1aa914d880
Markus KARG <markus@headcrashing.eu>
2018-05-26 14:28:21 +0200

    Java SE Bootstrap Example
    Signed-off-by: Markus KARG <markus@headcrashing.eu>

---
+++ examples/src/main/java/jaxrs/examples/bootstrap/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides examples for bootstrapping a JAX-RS application on Java SE.
+ * <p>
+ * Currently there only is a minimum example contained.
+ * </p>
+ *
+ * @since 2.2
+ * @see javax.ws.rs.JAXRS;
+ */
+package jaxrs.examples.bootstrap;
