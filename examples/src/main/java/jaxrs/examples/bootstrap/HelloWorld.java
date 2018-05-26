8816e46485d6ac239057de6f39427c9f270d5c47
Markus KARG <markus@headcrashing.eu>
2018-05-26 14:28:21 +0200

    Java SE Bootstrap Example
    Signed-off-by: Markus KARG <markus@headcrashing.eu>

---
+++ examples/src/main/java/jaxrs/examples/bootstrap/HelloWorld.java
@@ -0,0 +1,24 @@
+package jaxrs.examples.bootstrap;
+
+import java.util.Collections;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("helloworld")
+@Path("hello")
+public class HelloWorld extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return Collections.singleton(HelloWorld.class);
+    }
+
+    @GET
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
